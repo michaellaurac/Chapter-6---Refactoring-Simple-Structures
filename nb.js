3c08d07b17bcafefb3003580faa5e2145bc58349
Michaël L <michaellaurac@gmail.com>
2022-05-17 20:29:47 +0200

    Reinitialise initial commit

--- nb.js
+++ nb.js
@@ -7,65 +7,69 @@ iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
 babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
 creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
 army = ['ab', 'ebm7', 'dbadd9', 'fm7', 'bbm', 'abmaj7', 'ebm'];
-paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
-toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'];
+paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7',
+            'em7', 'a7', 'f7', 'b'];
+toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7',
+         'g7'];
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 song_11 = [];
 
-const songs = [];
-const labels = [];
-const allChords = [];
-const labelCounts = [];
-const labelProbabilities = [];
-const chordCountsInLabels = {};
-const probabilityOfChordsInLabels = {};
+var songs = [];
+var labels = [];
+var allChords = [];
+var labelCounts = [];
+var labelProbabilities = [];
+var chordCountsInLabels = {};
+var probabilityOfChordsInLabels = {};
 
 function train(chords, label){
   songs.push([label, chords]);
   labels.push(label);
-  for (let i = 0; i < chords.length; i++){
-    if (!allChords.includes(chords[i])){
+  for (var i = 0; i < chords.length; i++){
+    if(!allChords.includes(chords[i])){
       allChords.push(chords[i]);
     }
   }
-  if (!!(Object.keys(labelCounts).includes(label))){
-    labelCounts[Label] = labelCounts[label] + 1;
+  if(!!(Object.keys(labelCounts).includes(label))){
+    labelCounts[label] = labelCounts[label] + 1;
   } else {
     labelCounts[label] = 1;
   }
 };
 
 function getNumberOfSongs(){
-  return songs.length;
+   return songs.length;
 };
 
 function setLabelProbabilities(){
   Object.keys(labelCounts).forEach(function(label){
-    const numberOfSongs = getNumberOfSongs();
+    var numberOfSongs = getNumberOfSongs();
     labelProbabilities[label] = labelCounts[label] / numberOfSongs;
   });
 };
 
-function setChordCountsInLabel(){
+function setChordCountsInLabels(){
   songs.forEach(function(i){
-    if (chordCountsInLabels[i[0]] === undefined){
+    if(chordCountsInLabels[i[0]] === undefined){
       chordCountsInLabels[i[0]] = {};
     }
     i[1].forEach(function(j){
-      if (chordCountsInLabels[i[0][j]] > 0){
-        chordCountsInLabels[i[0][j]] = chordCountsInLabels[i[0][j]] + 1;
+      if(chordCountsInLabels[i[0]][j] > 0){
+        chordCountsInLabels[i[0]][j] =
+chordCountsInLabels[i[0]][j] + 1;
       } else {
-        chordCountsInLabels[i[0][j]] = 1;
+        chordCountsInLabels[i[0]][j] = 1;
       }
     });
   });
 }
 
-function setProbabilityChordsInLabels(){
+function setProbabilityOfChordsInLabels(){
   probabilityOfChordsInLabels = chordCountsInLabels;
   Object.keys(probabilityOfChordsInLabels).forEach(function(i){
     Object.keys(probabilityOfChordsInLabels[i]).forEach(function(j){
-      probabilityOfChordsInLabels[i][j] = probabilityOfChordsInLabels[i][j] * 1.0 / songs.length;
+      probabilityOfChordsInLabels[i][j] =
+probabilityOfChordsInLabels[i][j] * 1.0 / songs.length;
     });
   });
 }
@@ -81,18 +85,21 @@ train(toxic, 'hard');
 train(bulletproof, 'hard');
 
 setLabelProbabilities();
-setChordCountsInLabel();
-setProbabilityChordsInLabels();
+setChordCountsInLabels();
+setProbabilityOfChordsInLabels();
+
+
 
 function classify(chords){
-  const ttal = labelProbabilities;
+  var ttal = labelProbabilities;
   console.log(ttal);
-  const classified = {};
+  var classified = {};
   Object.keys(ttal).forEach(function(obj){
-    let first = labelProbabilities[obj] + 1.01;
+    var first = labelProbabilities[obj] + 1.01;
     chords.forEach(function(chord){
-      const probabilityOfChordInLabel = probabilityOfChordsInLabels[obj][chord];
-      if (probabilityOfChordInLabel === undefined){
+      var probabilityOfChordInLabel =
+probabilityOfChordsInLabels[obj][chord];
+      if(probabilityOfChordInLabel === undefined){
         first + 1.01;
       } else {
         first = first * (probabilityOfChordInLabel + 1.01);
@@ -101,7 +108,7 @@ function classify(chords){
     classified[obj] = first;
   });
   console.log(classified);
-}
+};
 
 classify(['d', 'g', 'e', 'dm']);
 classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
