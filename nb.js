84657a82a2262f6a05825ae19f33e01325db8877
Michaël L <michaellaurac@gmail.com>
2022-05-23 21:55:12 +0200

    Using concatenation with + sign

--- nb.js
+++ nb.js
@@ -96,6 +96,6 @@ function classify (chords) {
   console.log(classified);
 };
 
-console.log('Welcome to nb.js!');
+console.log('Welcome to ' + 'nb.js' + '!');
 classify(['d', 'g', 'e', 'dm']);
 classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
