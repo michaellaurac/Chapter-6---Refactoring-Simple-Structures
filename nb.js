8f0e6882e1bcdaeecc866d140a13a9c11f8cbc02
Michaël L <michaellaurac@gmail.com>
2022-05-23 21:54:18 +0200

    Logging a concatenated magic string at the beginning of the output.

--- nb.js
+++ nb.js
@@ -96,5 +96,6 @@ function classify (chords) {
   console.log(classified);
 };
 
+console.log('Welcome to nb.js!');
 classify(['d', 'g', 'e', 'dm']);
 classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
